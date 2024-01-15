72525d28f30364227d130e0d805c20d865d46862
Ricardo Saldarriaga <40249987+RicardoSaldarriagaPayco@users.noreply.github.com>
2024-01-15 17:21:27 -0500

    Update Response.php

--- src/Message/Response.php
+++ src/Message/Response.php
@@ -27,7 +27,8 @@ class Response extends AbstractResponse implements RedirectResponseInterface
 
     public function getRedirectUrl()
     {
-        return $this->getCheckoutEndpoint().'?'.http_build_query($this->getRedirectQueryParameters(), '', '&');
+        $data = $this->getRedirectQueryParameters();
+        return $this->getCheckoutEndpoint().'?'.http_build_query(["session" => $data], '', '&');
     }
 
     public function getRedirectMethod()
@@ -42,7 +43,17 @@ class Response extends AbstractResponse implements RedirectResponseInterface
 
     public function getTransactionReference()
     {
-        return $this->data ?? null;
+        $data = $this->data ?? null;
+        $public_key = $data["public_key"];
+        $private_key = $data["private_key"];
+        $headers = [
+            "apikey: ${public_key}",
+            "privatekey: ${private_key}",
+            'Content-Type: application/json'
+        ];
+        $data = $this->formatOmniPayPaylod($data);
+        $token = $this->makeApifyRequest($headers, $data);
+        return $token->success ? $token->data->sessionId : null;
     }
 
     protected function getRedirectQueryParameters()
@@ -68,5 +79,98 @@ class Response extends AbstractResponse implements RedirectResponseInterface
     protected function getCheckoutEndpoint()
     {
         return $this->endpoint;
+    }
+    
+    public function makeApifyRequest($headers,$data)
+    {
+        $url = "https://apify.epayco.io/checkout/payment/session";
+        try {
+            $jsonData = json_encode($data);
+            $curl = curl_init();
+            curl_setopt_array($curl, array(
+                CURLOPT_URL => $url,
+                CURLOPT_RETURNTRANSFER => true,
+                CURLOPT_ENCODING => '',
+                CURLOPT_MAXREDIRS => 10,
+                CURLOPT_TIMEOUT => 0,
+                CURLOPT_FOLLOWLOCATION => true,
+                CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+                CURLOPT_CUSTOMREQUEST => 'POST',
+                CURLOPT_POSTFIELDS =>$jsonData,
+                CURLOPT_HTTPHEADER => $headers,
+            ));
+            $resp = curl_exec($curl);
+            if ($resp === false) {
+                return array('curl_error' => curl_error($curl), 'curerrno' => curl_errno($curl));
+            }
+            curl_close($curl);
+            return json_decode($resp);
+
+        }catch (Exception $e) {
+            return [
+                "success" => false,
+                "titleResponse" => "error",
+                "textResponse" => $e->getMessage(),
+                "data" => []
+            ];
+        }
+    }
+    
+    public function formatOmniPayPaylod($data)
+    {  
+        if($data["test"] == '1'){
+            $testMode = "true";
+        }else{
+            $testMode = "false";
+        }
+        $data_ico = "0";
+        $data_hasCvv = "false";
+        $formattedData = array(
+            "test" => $testMode,
+            "extra1" => $data["transactionId"],
+            "extra2" => "0",
+            "extra5" => 'P49',
+            "name" =>$data["description"],
+            "description" =>$data["description"],
+            "invoice" => strval($data["transactionId"]),
+            "currency" => $data["currency"],
+            "amount" => strval($data["amount"]),
+            "tax_base" => strval($data["subTotal"]),
+            "tax" => strval($data["tax"]),
+            "taxIco" => $data_ico,
+            "country" => $data["country"],
+            "lang" => $data["lang"],
+            "external" => "true",
+            "confirmation" => $data["notifyUrl"],
+            "response" => $data["returnurl"],
+            "name_billing" => $data["firstName"] ." ". $data["lastName"],
+            "address_billing" => $data["address"],
+            "email_billing" => $data["email"],
+            "autoclick"=>"true",
+            "ip" => $this->getIp()
+        );
+       return $formattedData;
+    }
+    
+    public function getIp()
+    {
+        $ipaddress = '';
+        if (isset($_SERVER['HTTP_CLIENT_IP']))
+            $ipaddress = $_SERVER['HTTP_CLIENT_IP'];
+        else if(isset($_SERVER['HTTP_X_FORWARDED_FOR']))
+            $ipaddress = $_SERVER['HTTP_X_FORWARDED_FOR'];
+        else if(isset($_SERVER['HTTP_X_FORWARDED']))
+            $ipaddress = $_SERVER['HTTP_X_FORWARDED'];
+        else if(isset($_SERVER['HTTP_X_CLUSTER_CLIENT_IP']))
+            $ipaddress = $_SERVER['HTTP_X_CLUSTER_CLIENT_IP'];
+        else if(isset($_SERVER['HTTP_FORWARDED_FOR']))
+            $ipaddress = $_SERVER['HTTP_FORWARDED_FOR'];
+        else if(isset($_SERVER['HTTP_FORWARDED']))
+            $ipaddress = $_SERVER['HTTP_FORWARDED'];
+        else if(isset($_SERVER['REMOTE_ADDR']))
+            $ipaddress = $_SERVER['REMOTE_ADDR'];
+        else
+            $ipaddress = 'UNKNOWN';
+        return $ipaddress;
     }
 }
